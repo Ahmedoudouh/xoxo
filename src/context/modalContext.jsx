47dd296d83d635cf3d7e0ca11219eed91c070486
Ahmed Oudouh <oudouhahmed@gmail.com>
2022-04-19 17:32:47 +0000

    test

--- src/context/modalContext.jsx
+++ src/context/modalContext.jsx
@@ -1,26 +1,4 @@
-/*import { createContext, useState } from "react";
 
-const ModalContext = createContext();
-
-const ModalState = (props) => {
-  const [show, setShow] = useState(false);
-
-  const showModal = () => {
-    setShow(true);
-  };
-
-  const hideModal = () => {
-    setShow(false);
-  };
-
-  return (
-    <ModalContext.Provider value={{ show, showModal, hideModal }}>
-      {props.childern}
-    </ModalContext.Provider>
-  );
-};
-export {ModalContext}
-*/
 import { createContext, useState } from "react";
 const ModalContext = createContext();
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
 import { ModalState } from "./context/modalContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <ModalState>
-        <App />
+      <App />{" "}
     </ModalState>
   </React.StrictMode>,
   document.getElementById("root")
